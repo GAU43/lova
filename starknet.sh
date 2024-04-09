d1d694278b4553cbd312d17a6c439ec18cab387d
GAU43 <166529681+GAU43@users.noreply.github.com>
2024-04-09 17:59:30 +0300

    Update starknet.sh

--- starknet.sh
+++ starknet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://rpc.starknet.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://rpc.starknet.lava.build/lava-referer-282beb94-1ebd-4db1-babd-d4e98e8ab56b/"
 
 OUTPUT_DIR="logs"
 
